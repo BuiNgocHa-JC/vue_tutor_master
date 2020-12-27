c8628631b555f7679b7c7dd5c87999411bd74b84
BuiNgocHa-JC <52252088+BuiNgocHa-JC@users.noreply.github.com>
2020-12-27 12:13:33 +0700

    new commit

---
+++ src/components/photo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="photo.url" alt ="" />
+</template>
+<script>
+  export default {
+    name: 'photo',
+    props: ['photo']
+  }
+</script>
+<style scoped>
+  .column img {
+    vertical-align: middle;
+    width: 100%;
+  }
+</style>
