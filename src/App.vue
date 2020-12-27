5d412a076b023981fc40bfe17bfc706843bd6382
BuiNgocHa-JC <52252088+BuiNgocHa-JC@users.noreply.github.com>
2020-12-27 12:00:24 +0700

    Vue tutor no BE

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <action v-bind:message="messageAction"></action>
+    <grid v-bind:photos="photos"></grid>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import action from './components/action'
+  import grid from './components/grid'
+  export default {
+    name: 'App',
+    components: { action, grid },
+    data: function () {
+      return {
+        photos: [
+          {
+            id: 0,
+            url: "https://i.imgur.com/p3IKY9G.jpg"
+          }
+        ],
+        messageAction: "It's working !"
+      }
+    }
   }
-}
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    font-family: Arial;
+  }
 </style>
